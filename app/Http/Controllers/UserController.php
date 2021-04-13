f5c89cc8d5fee23ef9cf8a7f6185d359fe352cc9
Rhea Mae Baja <rheamaebaja010@gmail.com>
2021-04-13 14:00:12 +0800

    First Commit to Github Repo

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use Illuminate\Http\Request;
+    use App\User;
+
+    Class UserController extends Controller {
+        private $request;
+
+        public function __construct(Request $request){
+            $this->request = $request;
+        }
+
+        public function getUsers(){
+            $users = User::all();
+            return response()->json($users, 200);
+        }
+    }
+
+?>
